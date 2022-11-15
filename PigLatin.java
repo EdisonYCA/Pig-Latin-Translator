fa85f67f0bfb6e45e9d59d6b495a664a59d07d7e
Edison Caceres <moneymaneddie@Edisons-MacBook-Air.local>
2022-11-15 14:35:06 -0500

    Finish program.

--- PigLatin.java
+++ PigLatin.java
@@ -1,9 +1,65 @@
-
+import javax.swing.JOptionPane;
 public class PigLatin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String userString;
+		String translation = "";
+		
+		userString = JOptionPane.showInputDialog("Enter a sentence or word to translate: ");
+		
+		String[] words = userString.split(" ");
+		for(int i = 0; i < words.length; i++) {
+			translation += pigLatinTranslation(words[i]);
+		}
+		
+		JOptionPane.showMessageDialog(null, translation);
+		
 	}
-
+	
+	public static String pigLatinTranslation(String sentence) {
+		char[] temp = new char[sentence.length()];
+		for(int i = 0; i < temp.length; i++) {
+			temp[i] = sentence.toCharArray()[i];
+		}
+		
+		if(isVowel(temp[0])) {
+			return sentence += "-ay ";
+		}		
+		else {
+			
+			String strTemp = "";
+			String newTranslation = "";
+			int index = 0;
+			
+			for(int i = 0; i < temp.length; i++) {
+				if(!isVowel(temp[i])) {
+					strTemp += temp[i];
+					index++;
+				}
+				else {
+					break;
+				}
+			}
+			
+			for(int i = index; i < temp.length; i++) {
+				newTranslation += temp[i];
+			}
+			
+			newTranslation += "-";
+			newTranslation += strTemp;
+			newTranslation += "ay ";
+			return newTranslation;
+		}
+		
+	}
+	
+	public static boolean isVowel(char a) {
+		char[] vowels = {'A', 'E', 'I', 'O', 'U'};
+		for(int i = 0; i < vowels.length; i++) {
+			if(vowels[i] == Character.toUpperCase(a)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
